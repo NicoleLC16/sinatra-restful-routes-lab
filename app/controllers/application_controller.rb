146c898de602c631cd32a46e5a2b5d7ff3f7ee79
NicoleLC16 <asalwaysnicole@outlook.com>
2019-12-11 15:30:04 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,52 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+    redirect to '/recipes'
+  end
+
+  get '/recipes' do #index page
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do #new recipe form page
+    erb :new
+  end
+
+  post '/recipes' do #create new recipie 
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do #show page
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do #edit form page
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do #patch edit
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params[:name], 
+    params[:ingredients], 
+    params[:cook_time])
+    # @recipe = Recipe.find_by_id(params[:id])
+    # @recipe.name = params[:name]
+    # @recipe.ingredients = params[:ingredients]
+    # @recipe.cook_time = params[:cook_time]
+    # @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+    # binding.pry
+  end
+
+  delete '/recipes/:id' do #delete a recipe
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect "/recipes"
+  end
 
 end
